0fbccf2791837845d2046d50df6aa5fe1aaa0faf
rtong <rtong@groupon.com>
2015-11-08 19:17:24 -0800

    basic version

--- app/src/main/java/com/codepath/apps/twitter/TweetsArrayAdapter.java
+++ app/src/main/java/com/codepath/apps/twitter/TweetsArrayAdapter.java
@@ -1,9 +1,16 @@
 package com.codepath.apps.twitter;
 
 import android.content.Context;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.codepath.apps.twitter.models.Tweet;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -13,7 +20,29 @@ import java.util.List;
 public class TweetsArrayAdapter extends ArrayAdapter<Tweet>{
 
     public TweetsArrayAdapter(Context context, List<Tweet> tweets) {
-        super(context, android.R.layout.simple_list_item_1, tweets);
+        super(context, 0, tweets);
     }
     //override and setup custom template
+    //ViewHolder pattern
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // 1 get the tweet
+        Tweet tweet = getItem(position);
+        // find or inflate the template
+        if(convertView == null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_tweet, parent, false);
+        }
+        // find the sub views to fill with data in the template
+        ImageView ivProfileImage = (ImageView) convertView.findViewById(R.id.ivProfileImage);
+        TextView tvUserName = (TextView) convertView.findViewById(R.id.tvUserName);
+        TextView tvBody = (TextView) convertView.findViewById(R.id.tvBody);
+        //populate data into subviews
+        tvUserName.setText(tweet.getUser().getScreenNme());
+        tvBody.setText(tweet.getBody());
+        ivProfileImage.setImageResource(android.R.color.transparent);//clear out the old image for a recycled view
+        Picasso.with(getContext()).load(tweet.getUser().getProfileImageUrl()).into(ivProfileImage);
+        // return the view to be inserted into the list
+        return convertView;
+
+    }
 }
